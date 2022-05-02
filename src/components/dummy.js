c7a2f2b975d6e0a9bb9b640f63f4ba7cb01df08d
WarrenBlack102 <warrenblack102@outlook.com>
2022-05-02 15:36:58 -0400

    second commit

---
+++ src/components/dummy.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Dummy extends Component {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>PortfolioDetails</h2>
+            </div>
+        )
+    }
+}
